3fcfcfd8676627cc00edab58720df7335868e47e
Lars Michaelis <mail@larmic.de>
2015-06-25 06:33:58 +0200

    BUT-311 Step 1: Create simple component

---
+++ components/src/main/java/de/larmic/butterfaces/component/html/HtmlDocumentation.java
@@ -0,0 +1,29 @@
+package de.larmic.butterfaces.component.html;
+
+import de.larmic.butterfaces.component.base.component.UIComponentBase;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.FacesComponent;
+
+@ResourceDependencies({
+        @ResourceDependency(library = "butterfaces-css", name = "butterfaces-section.css", target = "head"),
+        @ResourceDependency(library = "butterfaces-configurable", name = "bootstrap.min.css", target = "head") })
+@FacesComponent(HtmlDocumentation.COMPONENT_TYPE)
+public class HtmlDocumentation extends UIComponentBase {
+
+	public static final String COMPONENT_TYPE = "de.larmic.butterfaces.component.documentation";
+	public static final String COMPONENT_FAMILY = "de.larmic.butterfaces.component.family";
+	public static final String RENDERER_TYPE = "de.larmic.butterfaces.renderkit.html_basic.DocumentationRenderer";
+
+	public HtmlDocumentation() {
+		super();
+		this.setRendererType(RENDERER_TYPE);
+	}
+
+	@Override
+	public String getFamily() {
+		return COMPONENT_FAMILY;
+	}
+
+}
---
+++ components/src/main/java/de/larmic/butterfaces/component/renderkit/html_basic/DocumentationRenderer.java
@@ -0,0 +1,46 @@
+package de.larmic.butterfaces.component.renderkit.html_basic;
+
+import de.larmic.butterfaces.component.base.renderer.HtmlBasicRenderer;
+import de.larmic.butterfaces.component.html.HtmlDocumentation;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.render.FacesRenderer;
+import java.io.IOException;
+
+/**
+ * Created by larmic on 31.07.14.
+ */
+@FacesRenderer(componentFamily = HtmlDocumentation.COMPONENT_FAMILY, rendererType = HtmlDocumentation.RENDERER_TYPE)
+public class DocumentationRenderer extends HtmlBasicRenderer {
+
+    @Override
+    public void encodeBegin(final FacesContext context, final UIComponent component) throws IOException {
+        rendererParamsNotNull(context, component);
+
+        if (!shouldEncode(component)) {
+            return;
+        }
+
+        final ResponseWriter writer = context.getResponseWriter();
+
+        writer.startElement(ELEMENT_DIV, component); // component
+        writer.writeAttribute("class", "butter-component-documentation", "styleClass");
+    }
+
+
+    @Override
+    public void encodeEnd(final FacesContext context, final UIComponent component) throws IOException {
+        rendererParamsNotNull(context, component);
+
+        if (!shouldEncode(component)) {
+            return;
+        }
+
+        final ResponseWriter writer = context.getResponseWriter();
+
+        writer.endElement(ELEMENT_DIV); // component
+    }
+
+}
